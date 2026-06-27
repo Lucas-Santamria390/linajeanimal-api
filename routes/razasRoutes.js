93cd673bbeed5478abecf05fdb5b891a7d437347
xavier Gonzalez <xavier.gonzalez7@utp.ac.pa>
2026-06-27 16:41:52 -0500

    refactor(rutas): apuntar razasRoutes a deactivate

--- routes/razasRoutes.js
+++ routes/razasRoutes.js
@@ -48,7 +48,7 @@ router.delete('/:id',
   authorize('admin'),
   param('id').isMongoId().withMessage('ID inválido'),
   validarCampos,
-  controller.remove
+  controller.deactivate
 );
 
 module.exports = router;
